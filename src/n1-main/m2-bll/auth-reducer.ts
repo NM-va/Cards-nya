70e5b7ee9d36574bac333602abf6a833f0ff17f8
NM-va <nad.n.mwa@gmail.com>
2022-07-11 21:35:31 +0300

    delete old structure files

--- src/n1-main/m2-bll/auth-reducer.ts
+++
@@ -1,9 +0,0 @@
-const initialState: any = {};
-
-export const authReducer = (state:any = initialState, action: any): any => {
-    switch (action.type) {
-        
-        default:
-            return state
-    }
-}
--- src/n1-main/m2-bll/profile-reducer.ts
+++
@@ -1,9 +0,0 @@
-const initialState: any = {};
-
-export const profileReducer = (state:any = initialState, action: any): any => {
-    switch (action.type) {
-        
-        default:
-            return state
-    }
-}
