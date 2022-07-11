bb316d4fe35302df1b2eb04505cf9940c6636e52
NM-va <nad.n.mwa@gmail.com>
2022-07-11 21:40:41 +0300

    replacement HashRouter to BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
-import {Provider} from "react-redux";
 import './index.css';
-import App from './n1-main/App';
 import reportWebVitals from './reportWebVitals';
-import {store} from "./n1-main/m2-bll/store";
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import {store} from "./app/store";
+import { App } from './app/App';
 
 ReactDOM.render(
-  <React.StrictMode>
-      <HashRouter>
-          <Provider store={store}>
-              <App />
-          </Provider>
-      </HashRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+        <BrowserRouter>
+            <Provider store={store}>
+            <App/>
+            </Provider>
+        </BrowserRouter>,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
